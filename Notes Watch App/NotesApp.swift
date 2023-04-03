1881be00df9fe8de8ba98514ba3e7a9e03339233
Dzmitry Bladyka <bladykodmitry@gmail.com>
2023-04-04 01:48:58 +0300

    Initial Commit

---
+++ Notes Watch App/NotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesApp.swift
+//  Notes Watch App
+//
+//  Created by Dzmitry Bladyka on 4.04.23.
+//
+
+import SwiftUI
+
+@main
+struct Notes_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
